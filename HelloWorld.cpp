164cae97b76afcf59d3ae9eb7c1d673a6a6952e5
anderslt <47325164+anderslt@users.noreply.github.com>
2019-02-04 09:40:59 -0500

    Hello world

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+//Anders Love-Tedjoutomo Hello world program
+//Prints Helow world
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int age;
+    cout << "How old are you?" << endl;
+    cin >> age;
+
+    cout << "Hello world! you are" << age << "Years old" << endl;
+    return 0;
+}
